2fbd26e3ae9f1c300eb977fb0c106c426549a310
zhangzheng <zhangzheng1209@gmail.com>
2025-01-22 00:25:00 +0800

    Site updated: 2025-01-22 00:24:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/21/hello-world/"];function toRandomPost(){
+var posts=["2025/01/22/mdhelp/","2025/01/21/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
