444d883c426c3fc4de1690f01d5f68c3a5af3995
zhangzheng <zhangzheng1209@gmail.com>
2025-02-09 14:26:48 +0800

    Site updated: 2025-02-09 14:26:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/08/Kuwait City/","2025/01/21/hello-world/","2025/01/23/Kuwait/","2025/01/22/Rabat_2024/","2025/01/22/test/","2025/01/22/mdhelp/"];function toRandomPost(){
+var posts=["2025/02/08/Kuwait City/","2025/01/22/mdhelp/","2025/01/21/hello-world/","2025/02/09/nizwa_2024/","2025/01/22/Rabat_2024/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
